21d880c1a3bae52f6fe99f7eafc7186998c4c126
Yu Zhang <atomhust@gmail.com>
2015-10-07 10:48:12 -0700

    Added CC API verion for each Doc version
    Signed-off-by: Jonathan Berkhahn <jaberkha@us.ibm.com>
    Signed-off-by: Ben Watts <bwatts@pivotal.io>

--- main.rb
+++ main.rb
@@ -3,34 +3,34 @@ require 'net/http'
 require "open-uri"
 require "json"
 
-BUILD_IDS = {
-  219 => {"BUILD_ID" => 83029259, "CC_API_VERSION" => "2.37.0"}
-  # 218 => 79900811,
-  # 217 => 78125812,
-  # 215 => 74335342,
-  # 214 => 72920095,
-  # 213 => 69811325,
-  # 212 => 67117720,
-  # 211 => 65314847,
-  # 210 => 63419982,
-  # 209 => 62731063,
-  # 208 => 61698265,
-  # 207 => 58532799,
-  # 206 => 57530258,
-  # 205 => 55212903,
-  # 204 => 54949372,
-  # 203 => 53876287,
-  # 202 => 53235349,
-  # 201 => 52833659,
-  # 200 => 50698978,
-  # 199 => 50433011,
-  # 198 => 50144686,
-  # 197 => 48526348,
-  # 196 => 47595973,
-  # 195 => 44998082,
-  # 194 => 41997426,
-  # 193 => 40945705,
-  # 192 => 40015178
+API_VERSIONS = {
+  219 => {'BUILD_ID' => 83029259, 'CC_API_VERSION' => '2.37.0'},
+  218 => {'BUILD_ID' => 79900811, 'CC_API_VERSION' => '2.36.0'},
+  217 => {'BUILD_ID' => 78125812, 'CC_API_VERSION' => '2.35.0'},
+  215 => {'BUILD_ID' => 74335342, 'CC_API_VERSION' => '2.34.0'},
+  214 => {'BUILD_ID' => 72920095, 'CC_API_VERSION' => '2.33.0'},
+  213 => {'BUILD_ID' => 69811325, 'CC_API_VERSION' => '2.33.0'},
+  212 => {'BUILD_ID' => 67117720, 'CC_API_VERSION' => '2.29.0'},
+  211 => {'BUILD_ID' => 65314847, 'CC_API_VERSION' => '2.28.0'},
+  210 => {'BUILD_ID' => 63419982, 'CC_API_VERSION' => '2.27.0'},
+  209 => {'BUILD_ID' => 62731063, 'CC_API_VERSION' => '2.27.0'},
+  208 => {'BUILD_ID' => 61698265, 'CC_API_VERSION' => '2.25.0'},
+  207 => {'BUILD_ID' => 58532799, 'CC_API_VERSION' => '2.25.0'},
+  206 => {'BUILD_ID' => 57530258, 'CC_API_VERSION' => '2.24.0'},
+  205 => {'BUILD_ID' => 55212903, 'CC_API_VERSION' => '2.23.0'},
+  204 => {'BUILD_ID' => 54949372, 'CC_API_VERSION' => '2.23.0'},
+  203 => {'BUILD_ID' => 53876287, 'CC_API_VERSION' => '2.23.0'},
+  202 => {'BUILD_ID' => 53235349, 'CC_API_VERSION' => '2.22.0'},
+  201 => {'BUILD_ID' => 52833659, 'CC_API_VERSION' => '2.22.0'},
+  200 => {'BUILD_ID' => 50698978, 'CC_API_VERSION' => '2.22.0'},
+  199 => {'BUILD_ID' => 50433011, 'CC_API_VERSION' => '2.22.0'},
+  198 => {'BUILD_ID' => 50144686, 'CC_API_VERSION' => '2.22.0'},
+  197 => {'BUILD_ID' => 48526348, 'CC_API_VERSION' => '2.21.0'},
+  196 => {'BUILD_ID' => 47595973, 'CC_API_VERSION' => '2.21.0'},
+  195 => {'BUILD_ID' => 44998082, 'CC_API_VERSION' => '2.19.0'},
+  194 => {'BUILD_ID' => 41997426, 'CC_API_VERSION' => '2.18.0'},
+  193 => {'BUILD_ID' => 40945705, 'CC_API_VERSION' => '2.18.0'},
+  192 => {'BUILD_ID' => 40015178, 'CC_API_VERSION' => '2.17.0'}
 }
 
 def template
@@ -68,7 +68,7 @@ EOS
 end
 
 def latest_release_num
-  BUILD_IDS.keys.max
+  API_VERSIONS.keys.max
 end
 
 get '/' do
@@ -100,7 +100,7 @@ get %r{/(\d+)(/.*)?} do |version, docs_path|
   cf_release_version = version.to_i rescue nil
   docs_path = "/" unless docs_path
 
-  travis_build_id = BUILD_IDS[cf_release_version]["BUILD_ID"]
+  travis_build_id = API_VERSIONS[cf_release_version]["BUILD_ID"]
   s3_base_url = "https://s3.amazonaws.com/cc-api-docs/#{travis_build_id}"
 
   html_content = fetch_html_from_s3(s3_base_url, docs_path, cf_release_version)
@@ -117,7 +117,7 @@ def fetch_html_from_s3(s3_base_url, docs_path, cf_release_version)
   rescue => e
     if e.message =~ /not found/i
       halt 404, erb(template, locals: {
-        header: version_links_html(cf_release_version, BUILD_IDS.keys, docs_path),
+        header: version_links_html(cf_release_version, API_VERSIONS.keys, docs_path),
         content: "Not Found"
       })
     end
@@ -136,7 +136,7 @@ def modify_html(html_content, docs_path, cf_release_version)
   html_body = html_content.gsub(/.*<body>/, '').gsub(/<\/body>.*/, '')
   locals = {
     content: html_body,
-    header: version_links_html(cf_release_version, BUILD_IDS.keys, docs_path)
+    header: version_links_html(cf_release_version, API_VERSIONS.keys, docs_path)
   }
 
   erb template, locals: locals
@@ -152,18 +152,17 @@ def version_links_html(current_version, all_versions, current_path)
     release_candidate_link: link_for('release-candidate', current_version, current_path),
     runtime_passed_link: link_for('runtime-passed', current_version, current_path),
     current_version: current_version,
-    current_cc_api_version: get_cc_api_version(current_version)
+    current_cc_api_version: get_cc_api_version(current_version, true)
   }
 
   erb header_template, locals: locals
 end
 
-def get_cc_api_version(version)
-  if version.is_a?(Fixnum)
-    cc_api_version = "- CC API VERSION " + BUILD_IDS[version.to_i]["CC_API_VERSION"]
-  else
-    cc_api_version = ""
-  end
+def get_cc_api_version(version, bolded = false)
+  return "" unless version.is_a?(Fixnum)
+  num = API_VERSIONS[version.to_i]["CC_API_VERSION"]
+  prefix = bolded ? "<strong>- CC API VERSION</strong>" : "- CC API VERSION"
+  "#{prefix} #{num}"
 end
 
 def link_for(version, current_version, current_path)
