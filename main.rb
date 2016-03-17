7b138da82aa57ee0ba551df3848ba08db48cb185
Dieu Cao <dcao@pivotal.io>
2016-03-17 14:19:20 -0700

    Add v232

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require "open-uri"
 require "json"
 105020109
 API_VERSIONS = {
+  232 => {'BUILD_ID' => 115991017, 'CC_API_VERSION' => '2.52.0'},
   231 => {'BUILD_ID' => 110284023, 'CC_API_VERSION' => '2.51.0'},
   230 => {'BUILD_ID' => 105020109, 'CC_API_VERSION' => '2.48.0'},
   229 => {'BUILD_ID' => 103428675, 'CC_API_VERSION' => '2.47.0'},
